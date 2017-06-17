731f7596ae1d7190fdab373578c0b8dd32b10abe
liupan <liupan@foreveross.com>
2017-06-17 10:56:54 +0800

    优化样式

--- src/Browser.vue
+++ src/Browser.vue
@@ -1,141 +1,143 @@
 <template>
-  <div id='browser' class="browser-wrap">
-    <div class="browser-box" :class="{'has-web-bar':openData.showToolBar}">
-      <div class="web-bar web-top-bar">
-        <input type="text" name="url" v-model.lazy="url" v-show="!webTitleShow" @blur="webTitleShow = true">
-        <div class="web-title" v-show="webTitleShow" @click="webTitleShow = false">
-          <i class="v-icon bg-icon"></i>{{webTitle}}</div>
-      </div>
-      <iframe :src="openData.url" @load="loaded" ref="iframe"></iframe>
-      <div class="web-bar web-bottom-bar">
-        <span @click="back">
-          <i class="v-icon bg-back"></i>
-        </span>
-        <span @click="forward">
-          <i class="v-icon bg-forward"></i>
-        </span>
-        <span @click="reload" class="reload">
-          <i class="v-icon bg-reload"></i>
-        </span>
-      </div>
+    <div id='browser' class="browser-wrap">
+        <div class="browser-box" :class="{'has-web-bar':openData.showToolBar}">
+            <div class="web-bar web-top-bar">
+                <input type="text" name="url" v-model.lazy="url" v-show="!webTitleShow" @blur="webTitleShow = true">
+                <div class="web-title" v-show="webTitleShow" @click="webTitleShow = false">
+                    <i class="v-icon bg-icon"></i>{{webTitle}}</div>
+            </div>
+            <iframe :src="openData.url" @load="loaded" ref="iframe"></iframe>
+            <div class="web-bar web-bottom-bar">
+                <span @click="back">
+                    <i class="v-icon bg-back"></i>
+                </span>
+                <span @click="forward">
+                    <i class="v-icon bg-forward"></i>
+                </span>
+                <span @click="reload" class="reload">
+                    <i class="v-icon bg-reload"></i>
+                </span>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-  export default {
+export default {
     name: 'VBrowser',
     props: {
-      openData: {
-        default: {
-          title: '坚果空间',
-          url: 'http://liu-pan.cc',
-          showToolBar: 'auto'
-        },
-        type: Object
-      }
+        openData: {
+            default: {
+                title: '坚果空间',
+                url: 'http://liu-pan.cc',
+                showToolBar: 'auto'
+            },
+            type: Object
+        }
     },
     data: function () {
-      return {
-        loading: false,
-        url: '',
-        webTitle: '',
-        webTitleShow: false
-      }
+        return {
+            loading: false,
+            url: '',
+            webTitle: '',
+            webTitleShow: false
+        }
     },
     watch: {
-      url: function (val) {
-        console.log(val);
-        this.webTitleShow = true;
-        if (val.indexOf('http://') !== 0 && val.indexOf('https://') !== 0) {
-          this.url = 'http://' + val;
+        url: function (val) {
+            console.log(val);
+            this.webTitleShow = true;
+            if (val.indexOf('http://') !== 0 && val.indexOf('https://') !== 0) {
+                this.url = 'http://' + val;
+            }
         }
-      }
     },
     methods: {
-      loaded() {
-        // this.bindHashchange();
-        this.webTitleShow = true;
-        this.checkMeta();
-        this.getTitle();
-      },
-      back() {
-        let F = this.$refs.iframe;
-        let FW = F.contentWindow;
-        FW.history.back();
-      },
-      forward() {
-        let FW = this.getFw();
-        FW.history.forward();
-      },
-      reload() {
-        let FW = this.getFw();
-        FW.location.reload();
-      },
-      bindHashchange() {
-        let FW = this.getFw();
-        FW.addEventListener('hashchange', () => {
-          console.log(FW.location)
-          this.url = FW.location.href;
-        });
-      },
-      checkMeta() {
-        let FW = this.getFw();
-        let webApp = FW.document.querySelector('head meta[name="apple-mobile-web-app-capable"]');
-        // console.log(webApp);
-        if (this.openData.showToolBar === 'auto' && webApp) {
-          this.openData.showToolBar = false;
+        loaded() {
+            // this.bindHashchange();
+            this.webTitleShow = true;
+            this.checkMeta();
+            this.getTitle();
+        },
+        back() {
+            let F = this.$refs.iframe;
+            let FW = F.contentWindow;
+            FW.history.back();
+        },
+        forward() {
+            let FW = this.getFw();
+            FW.history.forward();
+        },
+        reload() {
+            let FW = this.getFw();
+            FW.location.reload();
+        },
+        bindHashchange() {
+            let FW = this.getFw();
+            FW.addEventListener('hashchange', () => {
+                console.log(FW.location)
+                this.url = FW.location.href;
+            });
+        },
+        checkMeta() {
+            let FW = this.getFw();
+            let webApp = FW.document.querySelector('head meta[name="apple-mobile-web-app-capable"]');
+            // console.log(webApp);
+            if (this.openData.showToolBar === 'auto' && webApp) {
+                this.openData.showToolBar = false;
+            }
+        },
+        getFw() {
+            let F = this.$refs.iframe;
+            let FW = F.contentWindow;
+            return FW;
+        },
+        getTitle() {
+            let F = this.$refs.iframe;
+            this.webTitle = F.contentWindow.document.title;
         }
-      },
-      getFw() {
-        let F = this.$refs.iframe;
-        let FW = F.contentWindow;
-        return FW;
-      },
-      getTitle() {
-        let F = this.$refs.iframe;
-        this.webTitle = F.contentWindow.document.title;
-      }
     },
     created() {
-      this.webTitle = this.openData.title;
+        this.webTitle = this.openData.title;
     }
-  }
+}
 
 </script>
 
 <style>
-  #vue-phone-model .browser-wrap {
+#vue-phone-model .browser-wrap {
     width: 100%;
     height: 100%;
     position: absolute;
-    top: 20px;
+    top: 0;
     left: 0;
+    right: 0;
+    bottom: 0;
     overflow: hidden;
     z-index: 100;
-  }
+}
 
-  #vue-phone-model .has-web-bar.browser-box {
+#vue-phone-model .has-web-bar.browser-box {
     padding: 35px 0 35px 0;
     /*padding: 35px 0 0 0;*/
     -webkit-box-sizing: border-box;
     -moz-box-sizing: border-box;
     box-sizing: border-box;
-  }
+}
 
-  #vue-phone-model .browser-box {
+#vue-phone-model .browser-box {
     width: 100%;
     height: 100%;
     overflow: hidden;
     background: #fff;
-  }
+}
 
 
-  #vue-phone-model .has-web-bar .web-bar {
+#vue-phone-model .has-web-bar .web-bar {
     display: block;
-  }
+}
 
-  #vue-phone-model .web-bar {
+#vue-phone-model .web-bar {
     display: none;
     width: 100%;
     height: 35px;
@@ -143,25 +145,25 @@
     -webkit-box-sizing: border-box;
     -moz-box-sizing: border-box;
     box-sizing: border-box;
-  }
+}
 
-  #vue-phone-model .web-top-bar {
+#vue-phone-model .web-top-bar {
     border-bottom: 1px solid #ccc;
     margin-top: -35px;
     padding: 3px 5px;
-  }
+}
 
-  #vue-phone-model .web-top-bar .web-title {
+#vue-phone-model .web-top-bar .web-title {
     font-size: 12px;
     line-height: 30px;
     text-overflow: ellipsis;
     overflow: hidden;
     white-space: nowrap;
     text-indent: 5px;
-  }
+}
 
-  #vue-phone-model .web-top-bar input,
-  #vue-phone-model .web-top-bar .web-title {
+#vue-phone-model .web-top-bar input,
+#vue-phone-model .web-top-bar .web-title {
     display: block;
     width: 100%;
     height: 100%;
@@ -173,27 +175,27 @@
     border: 1px solid #ccc;
     color: #92969f;
     font-weight: 300;
-  }
+}
 
-  #vue-phone-model input:focus,
-  #vue-phone-model input:active {
+#vue-phone-model input:focus,
+#vue-phone-model input:active {
     outline: none;
-  }
+}
 
-  #vue-phone-model .web-bottom-bar {
+#vue-phone-model .web-bottom-bar {
     border-top: 1px solid #ccc;
     margin-bottom: -35px;
-  }
+}
 
-  #vue-phone-model .web-bottom-bar span {
+#vue-phone-model .web-bottom-bar span {
     display: inline-block;
     width: 45px;
     text-align: center;
     margin-top: 5px;
     cursor: pointer;
-  }
+}
 
-  #vue-phone-model iframe {
+#vue-phone-model iframe {
     width: 100%;
     height: 100%;
     border: none;
@@ -201,39 +203,38 @@
     -webkit-box-sizing: border-box;
     -moz-box-sizing: border-box;
     box-sizing: border-box;
-  }
+}
 
-  #vue-phone-model .v-icon {
+#vue-phone-model .v-icon {
     width: 20px;
     height: 20px;
     display: inline-block;
     background-size: 100%;
     background-repeat: no-repeat;
-  }
+}
 
-  #vue-phone-model .bg-back {
+#vue-phone-model .bg-back {
     background-image: url('./img/icons.png');
     background-position: 0px 0px;
-  }
+}
 
-  #vue-phone-model .bg-forward {
+#vue-phone-model .bg-forward {
     background-image: url('./img/icons.png');
     background-position: 0px -20px;
-  }
+}
 
-  #vue-phone-model .bg-icon {
+#vue-phone-model .bg-icon {
     background-image: url('./img/icons.png');
     background-position: 0px -40px;
     margin-bottom: -5px;
-  }
+}
 
-  #vue-phone-model .bg-reload {
+#vue-phone-model .bg-reload {
     background-image: url('./img/icons.png');
     background-position: 0px -60px;
-  }
+}
 
-  #vue-phone-model .reload {
+#vue-phone-model .reload {
     float: right;
-  }
-
+}
 </style>
--- src/Phone.vue
+++ src/Phone.vue
@@ -1,107 +1,107 @@
 <template>
-  <div id="vue-phone-model">
-    <div class="phone-wrap" :class="phone">
-      <div class="phone-box">
-        <div class="phone-receiver-wrap">
-          <i class="phone-camera"></i>
-          <div class="phone-receiver"></div>
-        </div>
-        <div class="phone-button" @click="hide"></div>
-        <div class="phone-tool-bar">
-          <span class="time">{{time}}</span>
-          <span class="network">4G
-            <i class="power"></i>
-          </span>
-        </div>
-        <div class="phone-desktop" :style="desktopStyle">
-          <div class="app-icon-box" v-for="(item,$index) in list" @click="open(item)" :key="$index">
-            <div class="app-icon">
-              <img :src="item.icon">
+    <div id="vue-phone-model">
+        <div class="phone-wrap" :class="phone">
+            <div class="phone-box">
+                <div class="phone-receiver-wrap">
+                    <i class="phone-camera"></i>
+                    <div class="phone-receiver"></div>
+                </div>
+                <div class="phone-button" @click="hide"></div>
+                <div class="phone-tool-bar">
+                    <span class="time">{{time}}</span>
+                    <span class="network">4G
+                        <i class="power"></i>
+                    </span>
+                </div>
+                <div class="phone-desktop" :style="desktopStyle">
+                    <div class="app-icon-box" v-for="(item,$index) in list" @click="open(item)" :key="$index">
+                        <div class="app-icon">
+                            <img :src="item.icon">
+                        </div>
+                        <p>{{item.title}}</p>
+                    </div>
+                    <transition name="v-zoom">
+                        <v-browser v-show="opened" :open-data="openData"></v-browser>
+                    </transition>
+                </div>
             </div>
-            <p>{{item.title}}</p>
-          </div>
-          <transition name="v-zoom">
-            <v-browser v-show="opened" :open-data="openData"></v-browser>
-          </transition>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
-  import VBrowser from './Browser.vue';
-  export default {
+import VBrowser from './Browser.vue';
+export default {
     name: 'v-phone',
     components: {
-      VBrowser
+        VBrowser
     },
     props: {
-      phone: {
-        default: 'i5',
-        type: String
-      },
-      list: {
-        default: () => {
-          return [{
-            title: '坚果空间',
-            url: 'http://liu-pan.cc',
-            showToolBar: true,
-            open: false
-          }]
+        phone: {
+            default: 'i5',
+            type: String
         },
-        type: Array
-      }
+        list: {
+            default: () => {
+                return [{
+                    title: '坚果空间',
+                    url: 'http://liu-pan.cc',
+                    showToolBar: true,
+                    open: false
+                }]
+            },
+            type: Array
+        }
     },
     data() {
-      return {
-        opened: false,
-        openData: {},
-        desktopStyle: {},
-        time: ':'
-      }
+        return {
+            opened: false,
+            openData: {},
+            desktopStyle: {},
+            time: ':'
+        }
     },
     methods: {
-      hide() {
-        this.opened = false;
-      },
-      open(item) {
-        this.openData = item;
-        this.opened = item.open = true;
-      },
-      checkTime(i) {
-        if (i < 10) {
-          i = "0" + i;
+        hide() {
+            this.opened = false;
+        },
+        open(item) {
+            this.openData = item;
+            this.opened = item.open = true;
+        },
+        checkTime(i) {
+            if (i < 10) {
+                i = "0" + i;
+            }
+            return i;
+        },
+        timer() {
+            let now = new Date();
+            let h = this.checkTime(now.getHours());
+            let m = this.checkTime(now.getMinutes());
+            this.time = h + ":" + m;
+            setTimeout(() => {
+                this.timer();
+            }, 1000 * 60);
         }
-        return i;
-      },
-      timer() {
-        let now = new Date();
-        let h = this.checkTime(now.getHours());
-        let m = this.checkTime(now.getMinutes());
-        this.time = h + ":" + m;
-        setTimeout(() => {
-          this.timer();
-        }, 1000 * 60);
-      }
     },
     created() {
-      this.timer();
-      this.list.some((item) => {
-        if (item.open === true) {
-          this.open(item);
-          //   console.log(item.title);
-          return true;
-        }
-      });
+        this.timer();
+        this.list.some((item) => {
+            if (item.open === true) {
+                this.open(item);
+                //   console.log(item.title);
+                return true;
+            }
+        });
     }
 
-  }
+}
 
 </script>
 
 <style>
-  #vue-phone-model .phone-wrap {
+#vue-phone-model .phone-wrap {
     border-bottom: 75px solid #383735;
     border-top: 50px solid #383735;
     border-left: 15px solid #383735;
@@ -110,22 +110,22 @@
     width: 320px;
     height: 568px;
     box-sizing: content-box;
-  }
+}
 
-  #vue-phone-model .phone-wrap.i6 {
+#vue-phone-model .phone-wrap.i6 {
     width: 375px;
     height: 667px;
-  }
+}
 
-  #vue-phone-model .phone-receiver-wrap {
+#vue-phone-model .phone-receiver-wrap {
     position: absolute;
     width: 100%;
     z-index: 5;
     top: -25px;
     text-align: center;
-  }
+}
 
-  #vue-phone-model .phone-camera {
+#vue-phone-model .phone-camera {
     width: 6px;
     height: 6px;
     border-radius: 30px;
@@ -134,9 +134,9 @@
     margin-left: 40%;
     margin-top: 1px;
     vertical-align: middle;
-  }
+}
 
-  #vue-phone-model .phone-receiver {
+#vue-phone-model .phone-receiver {
     background: #1c1d22;
     width: 52px;
     height: 6px;
@@ -144,9 +144,9 @@
     margin-left: 6px;
     border-radius: 50px;
     box-shadow: 1px 2px #52514e;
-  }
+}
 
-  #vue-phone-model .phone-button {
+#vue-phone-model .phone-button {
     position: absolute;
     width: 50px;
     height: 50px;
@@ -158,9 +158,9 @@
     z-index: 9;
     cursor: pointer;
     box-shadow: inset 0px 0px 2px 1px rgba(111, 111, 111, 0.4);
-  }
+}
 
-  #vue-phone-model .phone-tool-bar {
+#vue-phone-model .phone-tool-bar {
     width: 100%;
     height: 20px;
     background: #000;
@@ -175,17 +175,17 @@
     border-top: 2px solid #282828;
     border-bottom: none;
     margin-top: -20px;
-  }
+}
 
-  #vue-phone-model .phone-tool-bar .time {
+#vue-phone-model .phone-tool-bar .time {
     float: left;
-  }
+}
 
-  #vue-phone-model .phone-tool-bar .network {
+#vue-phone-model .phone-tool-bar .network {
     float: right;
-  }
+}
 
-  #vue-phone-model .phone-tool-bar .power {
+#vue-phone-model .phone-tool-bar .power {
     position: relative;
     display: inline-block;
     width: 20px;
@@ -193,9 +193,9 @@
     border-radius: 5px;
     overflow: hidden;
     border: 1px solid #fff;
-  }
+}
 
-  #vue-phone-model .phone-tool-bar .power::before {
+#vue-phone-model .phone-tool-bar .power::before {
     content: '';
     display: inline-block;
     width: 80%;
@@ -204,9 +204,9 @@
     position: absolute;
     top: 0;
     right: -1px;
-  }
+}
 
-  #vue-phone-model .phone-box {
+#vue-phone-model .phone-box {
     position: relative;
     width: 100%;
     height: 100%;
@@ -217,39 +217,40 @@
     padding-top: 20px;
     border: 2px solid #282828;
     border-top: none;
-  }
+}
 
-  #vue-phone-model .phone-desktop {
+#vue-phone-model .phone-desktop {
     width: 100%;
     height: 100%;
+    position: relative;
     background-image: url('./img/moon.jpg');
     background-color: cadetblue;
     background-size: cover;
     background-repeat: no-repeat;
-  }
+}
 
-  #vue-phone-model .app-icon-box {
+#vue-phone-model .app-icon-box {
     width: 17%;
     overflow: hidden;
     margin: 7% 4% 2% 4%;
     float: left;
     cursor: pointer;
-  }
+}
 
-  #vue-phone-model .app-icon-box .app-icon {
+#vue-phone-model .app-icon-box .app-icon {
     width: 100%;
     background: #fff;
     border-radius: 3px;
     overflow: hidden;
-  }
+}
 
-  #vue-phone-model .app-icon-box .app-icon img {
+#vue-phone-model .app-icon-box .app-icon img {
     width: 100%;
     display: block;
     border: none;
-  }
+}
 
-  #vue-phone-model .app-icon-box p {
+#vue-phone-model .app-icon-box p {
     width: 100%;
     margin: 5px 0 0 0;
     color: #fff;
@@ -261,19 +262,18 @@
     overflow: hidden;
     text-overflow: ellipsis;
     white-space: nowrap;
-  }
+}
 
-  #vue-phone-model .v-zoom-enter {
+#vue-phone-model .v-zoom-enter {
     opacity: 0;
     transform: scale3d(.3, .3, .3);
     -webkit-transform: scale3d(.3, .3, .3);
     -ms-transform: scale3d(.3, .3, .3);
-  }
+}
 
-  #vue-phone-model .v-zoom-enter-active {
+#vue-phone-model .v-zoom-enter-active {
     transition: transform .3s;
     -webkit-transition: transform .3s;
     -ms-transition: transform .3s;
-  }
-
+}
 </style>
