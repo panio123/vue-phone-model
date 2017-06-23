eb284d47bc36ac7b81dccadc5fd7a34e10b59ae3
liupan <liupan@foreveross.com>
2017-06-23 23:45:11 +0800

    1、更新示例图

--- src/App.vue
+++ src/App.vue
@@ -5,38 +5,37 @@
 </template>
 
 <script>
-  // import VPhone from './Phone.vue';
-  import logo from './img/mi_logo.jpg';
-  import VPhone from '../dist/phone.js';
-  import '../dist/phone.css';
-  // console.log(VPhone);
-  export default {
-    name: 'app',
-    components: {
-      VPhone
-    },
-    data() {
-      return {
-        list: [{
-          title: '坚果空间',
-          icon: logo,
-          url: 'http://liu-pan.cc',
-          showToolBar: true,
-          open: false
-        }, {
-          title: '小米商城',
-          icon: logo,
-          url: 'http://liu-pan.cc/example/mi_app/index.html',
-          showToolBar: true,
-          open: false
-        }]
-      }
+// import VPhone from './Phone.vue';
+import logo from './img/mi_logo.jpg';
+import VPhone from '../dist/phone.js';
+import '../dist/phone.css';
+// console.log(VPhone);
+export default {
+  name: 'app',
+  components: {
+    VPhone
+  },
+  data() {
+    return {
+      list: [{
+        title: '坚果空间',
+        icon: logo,
+        url: 'http://liu-pan.cc',
+        showToolBar: false,
+        open: false
+      }, {
+        title: '小米商城',
+        icon: logo,
+        url: 'http://liu-pan.cc/example/mi_app/index.html',
+        showToolBar: false,
+        open: false
+      }]
     }
   }
+}
 
 </script>
 
 <style>
-
 
 </style>
