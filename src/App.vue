91c7261f4099d3898545dd13402057f074a1d7e6
liupan <liupan@foreveross.com>
2017-07-04 14:20:51 +0800

    1、添加gif

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    <v-phone :list="list" background='#fff' type="i6"></v-phone>
+    <button @click="changeType('i5')">iPhone 5</button>
+    <button @click="changeType('i6')">iPhone 6</button>
+    <button @click="changeType('ipad')">ipad</button>
+    <button @click="changeBg('red')">红色背景</button>
+    <button @click="changeBg('green')">绿色背景</button>
+    <button @click="changeBg('')">重置</button>
+    <button @click="pushItme">新增</button>
+    <v-phone :list="list" :background='background' :type="phoneType"></v-phone>
   </div>
 </template>
 
@@ -9,7 +16,12 @@
 import logo from './img/mi_logo.jpg';
 import VPhone from '../dist/phone.js';
 import '../dist/phone.css';
-// console.log(VPhone);
+let item = {
+  title: '小米商城',
+  icon: logo,
+  url: 'http://liu-pan.cc/example/mi_app/index.html',
+  open: false
+};
 export default {
   name: 'app',
   components: {
@@ -17,6 +29,8 @@ export default {
   },
   data() {
     return {
+      phoneType: 'i5',
+      background: '',
       list: [{
         title: '坚果空间',
         url: 'http://liu-pan.cc',
@@ -29,11 +43,24 @@ export default {
         open: false
       }]
     }
+  },
+  methods: {
+    changeType(type) {
+      this.phoneType = type || 'i5';
+    },
+    pushItme() {
+      this.list.push(item);
+    },
+    changeBg(color) {
+      this.background = color || '';
+    }
   }
 }
 
 </script>
 
 <style>
-
+button {
+  margin: 10px;
+}
 </style>
